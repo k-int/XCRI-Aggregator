03e6fa20b5f08048d31c6c94fde92848e39b869c
Ian Ibbotson <ianibbo@googlemail.com>
2011-12-26 16:33:18 +0000

    Prep for end user app

--- XCRISearch/grails-app/conf/UrlMappings.groovy
+++ XCRISearch/grails-app/conf/UrlMappings.groovy
@@ -1,13 +1,14 @@
 class UrlMappings {
 
-  static mappings = {
-    "/$controller/$action?/$id?"{
-      constraints {
-        // apply constraints here
-      }
-    }
+  // static mappings = {
+  //   "/$controller/$action?/$id?"{
+  //     constraints {
+  //       // apply constraints here
+  //     }
+  //   }
 
     "/" (controller:"search", action:"index")
+    "/course/$id" (controller:"course", action:"index")
 
     "500"(view:'/error')
   }
--- XCRISearch/grails-app/controllers/com/k_int/xcri/CourseController.groovy
+++ XCRISearch/grails-app/controllers/com/k_int/xcri/CourseController.groovy
@@ -2,5 +2,39 @@ package com.k_int.xcri
 
 class CourseController {
 
-    def index() { }
+  def index() { 
+    log.debug("Course controller, params.id=${params.id}")
+
+    def result = [:]
+
+    // Get hold of some services we might use ;)
+    def mongo = new com.gmongo.GMongo();
+    def db = mongo.getDB("xcri")
+    org.elasticsearch.groovy.node.GNode esnode = ESWrapperService.getNode()
+    org.elasticsearch.groovy.client.GClient esclient = esnode.getClient()
+
+    if ( params.id != null ) {
+      // Form passed in a query
+
+      def search = esclient.search {
+        indices "courses"
+        types "course"
+        source {
+          query {
+            term(_id:params.id)
+          }
+        }
+      }
+
+      log.debug("Search returned $search.response.hits.totalHits total hits")
+      log.debug("First hit course is $search.response.hits[0]")
+      result.searchresult = search.response
+      render(view:'results',model:result)
+    }
+    else {
+      log.warn("No query.. Show search page")
+      render(view:'notfound',model:result)
+    }
+
+  }
 }
