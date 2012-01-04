ec6f018dcd15a9a300d68d0b18ac342d1a4ec811
Mark <Mark@K-Int.com>
2012-01-04 15:22:11 +0000

    user home now restricts to only show feeds assigned to user

--- FeedManager/grails-app/controllers/feedmanager/HomeController.groovy
+++ FeedManager/grails-app/controllers/feedmanager/HomeController.groovy
@@ -1,14 +1,21 @@
 package feedmanager
 
+import org.apache.shiro.SecurityUtils
+import com.k_int.feedmanager.*
+
 class HomeController {
 
   def index() {
+	  	
+	ShiroUser userInstance = ShiroUser.findByUsername(SecurityUtils.subject?.principal);
+	  
     params.max = Math.min(params.max ? params.int('max') : 10, 100)
 	
 	def results = com.k_int.feedmanager.Datafeed.createCriteria().list(max: params.max, offset: params.offset)
 	{
 		and
 		{
+			eq("owner.id", userInstance.id)
 			if(params.sort && params.order) { order(params.sort, params.order)}
 		}
 	}
