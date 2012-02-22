1b569775341fd97b2e791f9d09a48cc87748309a
ianibo <ianibbo@googlemail.com>
2012-02-22 11:54:03 +0000

    Cleaned up install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 
-cd /home/xcri/apache-tomcat-7.0.23/bin
+# export tc=/home/xcri/apache-tomcat-7.0.23/bin
+export tc=~/apache-tomcat
+
+cd $tc/bin
 # ./shutdown sh
 rm -Rf ../webapps/FeedManager ../webapps/HandlerRegistry ../webapps/repository ../webapps/discover
 rm -Rf ../logs/*
@@ -31,8 +34,8 @@ cd ~/src/XCRI-Aggregator/FeedManager
 grails prod war
 
 
-cp ~/src/XCRI-Aggregator/FeedManager/target/FeedManager-0.1.war ~/apache-tomcat-7.0.23/webapps/FeedManager.war
-cp ~/src/aggregator/repository/target/repository-0.1.war ~/apache-tomcat-7.0.23/webapps/repository.war
-cp ~/src/aggregator/HandlerRegistry/target/HandlerRegistry-0.1.war ~/apache-tomcat-7.0.23/webapps/HandlerRegistry.war
-cp ~/src/XCRI-Aggregator/XCRISearch/target/XCRISearch-0.1.war ~/apache-tomcat-7.0.23/webapps/discover.war
+cp ~/src/XCRI-Aggregator/FeedManager/target/FeedManager-0.1.war $tc/webapps/FeedManager.war
+cp ~/src/aggregator/repository/target/repository-0.1.war $tc/webapps/repository.war
+cp ~/src/aggregator/HandlerRegistry/target/HandlerRegistry-0.1.war $tc/webapps/HandlerRegistry.war
+cp ~/src/XCRI-Aggregator/XCRISearch/target/XCRISearch-0.1.war $tc/webapps/discover.war
 
