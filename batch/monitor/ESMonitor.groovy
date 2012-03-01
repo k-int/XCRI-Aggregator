0e350b58810691c3051d4480a8ecb18d48aae25e
ianibo <ianibbo@googlemail.com>
2012-03-01 14:29:02 +0000

    Get first 50 last modified courses

--- batch/monitor/ESMonitor.groovy
+++ batch/monitor/ESMonitor.groovy
@@ -3,17 +3,30 @@
 @GrabResolver(name='es', root='https://oss.sonatype.org/content/repositories/releases')
 // @Grab(group='com.gmongo', module='gmongo', version='0.9.2')
 @Grapes([
-  @Grab(group='net.sf.opencsv', module='opencsv', version='2.0'),
-  @Grab(group='org.elasticsearch', module='elasticsearch', version='0.19.0.RC2'),
-  @Grab(group='org.elasticsearch', module='elasticsearch-lang-groovy', version='1.1.0')])
-
-import org.apache.log4j.*
-
-LogManager.rootLogger.level = Level.DEBUG
+  @Grab(group='org.apache.httpcomponents', module='httpmime', version='4.1.2'),
+  @Grab(group='com.gmongo', module='gmongo', version='0.9.2'),
+  @Grab(group='org.apache.httpcomponents', module='httpclient', version='4.0')
+])
 
 def starttime = System.currentTimeMillis();
 
 // def l = new Loader();
 // l.load('uk_gaz_with_geo-2011-05-08.csv');
-// println("Gaz load completed in ${System.currentTimeMillis() - starttime}ms");
+println("ES Monitor run completed");
 
+def next=true;
+while(next) {
+  def mongo = new com.gmongo.GMongo()
+  def db = mongo.getDB("xcri")
+
+  def batch = db.courses.find( [ lastModified : [ $gt : 0 ] ] ).limit(51);
+
+  def i=0;
+  batch.each { r ->
+    println("* ${i++}");
+  }
+
+  println("batch: ${batch}");
+
+  next=false;
+}
