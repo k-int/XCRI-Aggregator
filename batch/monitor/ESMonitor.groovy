78f1febe1ef3099ee646ea212be870dffdc3fb60
ianibo <ianibbo@googlemail.com>
2012-03-02 16:43:10 +0000

    Working update if 4s

--- batch/monitor/ESMonitor.groovy
+++ batch/monitor/ESMonitor.groovy
@@ -5,19 +5,24 @@
 @Grapes([
   @Grab(group='org.apache.httpcomponents', module='httpmime', version='4.1.2'),
   @Grab(group='com.gmongo', module='gmongo', version='0.9.2'),
-  @Grab(group='org.apache.httpcomponents', module='httpclient', version='4.0')
+  @Grab(group='org.apache.httpcomponents', module='httpclient', version='4.0'),
+  @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.5.0')
 ])
 
 def starttime = System.currentTimeMillis();
+println("Startup.. Initialising at ${starttime}");
 
 def monitor = new RepoMonitor();
+def tse = new TripleStoreService();
 
-// def l = new Loader();
-// l.load('uk_gaz_with_geo-2011-05-08.csv');
-println("ES Monitor run completed");
+println("initialise triple store");
+tse.init();
 
+println("Connect to mongo");
 def mongo = new com.gmongo.GMongo()
 def db = mongo.getDB("xcri")
+
+println("Monitor starting after ${System.currentTimeMillis() - starttime}");
 
 monitor.iterateLatest(db,'courses', -1) { jsonobj ->
   println("buildRDFXML");
@@ -31,22 +36,27 @@ monitor.iterateLatest(db,'courses', -1) { jsonobj ->
   //
   // see http://svn.cetis.ac.uk/xcri/trunk/bindings/rdf/xcri_rdfs.xml
   //
-  xml.'rdf:RDF'('xmlns':'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
-                'xmlns:dcterms':'http://purl.org/dc/terms/',
-                'xmlns:dc':'http://purl.org/dc/elements/1.1/',
-                'xmlns:xcri':'http://xcri.org/profiles/catalog/1.2/') {
-    'rdf:resource'('rdf:about':"urn:xcri:course:${jsonobj._id}") {
-      'dc:title'(jsonobj.title)
-      'xcri:provid'(jsonobj.provid)
-      'xcri:provname'(jsonobj.provname)
-      'xcri:uri'(jsonobj.url)
-      'xcri:abstract'(jsonobj.description)
-    }
+  // xml.'rdf:RDF'('xmlns:rdf':'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+  //            'xmlns:dcterms':'http://purl.org/dc/terms/',
+  //            'xmlns:dc':'http://purl.org/dc/elements/1.1/',
+  //            'xmlns:xcri':'http://xcri.org/profiles/catalog/1.2/') {
+  //            'rdf:resource'('rdf:about':"urn:xcri:course:${jsonobj._id}") {
+  xml.'xcri:course'('rdf:about':"urn:xcri:course:${jsonobj._id}",
+                    'xmlns:rdf':'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+                    'xmlns:dcterms':'http://purl.org/dc/terms/',
+                    'xmlns:dc':'http://purl.org/dc/elements/1.1/',
+                    'xmlns:xcri':'http://xcri.org/profiles/catalog/1.2/') {
+    'dc:title'(jsonobj.title)
+    'xcri:provid'(jsonobj.provid)
+    'xcri:provname'(jsonobj.provname)
+    'xcri:uri'(jsonobj.url)
+    'xcri:abstract'(jsonobj.description)
   }
 
   def result = writer.toString();
-
-  // println(result);
+  tse.removeGraph("urn:xcri:course:${jsonobj._id}");
+  tse.update(result, "urn:xcri:course:${jsonobj._id}", 'application/rdf');
+  println("Updated ( ${System.currentTimeMillis() - starttime} )");
 }
 
-println("Completed");
+println("Completed after ${System.currentTimeMillis() - starttime}");
---
+++ batch/monitor/TripleStoreService.groovy
@@ -0,0 +1,88 @@
+import groovy.util.slurpersupport.GPathResult
+import static groovyx.net.http.ContentType.URLENC
+import static groovyx.net.http.ContentType.*
+import static groovyx.net.http.Method.*
+import groovyx.net.http.*
+import org.apache.http.entity.mime.*
+import org.apache.http.entity.mime.content.*
+import java.nio.charset.Charset
+import grails.converters.*
+import java.security.MessageDigest
+
+
+class TripleStoreService {
+
+  def fourstore_endpoint = null;
+  
+  @javax.annotation.PostConstruct
+  def init() {
+    println("init...");
+    if ( fourstore_endpoint == null ) {
+      fourstore_endpoint = new HTTPBuilder( 'http://localhost:9000/' )
+    }
+    else {  
+      println("init already called");
+    }
+    // fourstore_endpoint.auth.basic feed_definition.target.identity, feed_definition.target.credentials
+    // this.updateURL = new URL(baseurl + "/data/");
+  }
+
+  def update(graph, graph_uri, mimetype) { // throws MalformedURLException, ProtocolException, IOException {
+
+    if ( fourstore_endpoint == null )
+      throw new RuntimeException("not initialised");
+
+    try {
+      println("update ${graph_uri} - upload graph ${graph}");
+
+      fourstore_endpoint.request(POST) { request ->
+        // requestContentType = 'multipart/form-data'
+        requestContentType = 'application/x-www-form-urlencoded'
+
+        uri.path = 'data/'
+
+        body =  [ 
+          'mime-type' : 'mimetype' , 
+          'graph' : graph_uri,
+          'data' : graph
+          // 'graph' : URLEncoder.encode(graph_uri, "UTF-8"),
+          // 'data' : URLEncoder.encode(graph, "UTF-8")
+        ] 
+
+        response.success = { resp, data ->
+          println("response status: ${resp.statusLine} ${data}")
+        }
+
+        response.failure = { resp ->
+          println("Failure - ${resp.statusLine} ${resp}");
+        }
+      }
+    }
+    catch ( Exception e ) {
+      e.printStackTrace();
+    }
+    finally {
+      println("Complete");
+    }
+  }
+
+  def removeGraph(graph_uri) {
+    // URL delete_url = new URL(baseurl + "/data/"+uri);
+    // HttpURLConnection connection = (HttpURLConnection) this.updateURL.openConnection();
+    // connection.setDoOutput(true);
+    // connection.setRequestMethod("DELETE");
+    // connection.connect();
+    println("Delete ${graph_uri}");
+    fourstore_endpoint.request(DELETE) { request ->
+      uri.path = "data/${graph_uri}"
+
+      response.success = { resp, data ->
+        println("response status: ${resp.statusLine} ${data}")
+      }
+
+      response.failure = { resp ->
+        println("Failure - ${resp.statusLine} ${resp}");
+      }
+    }
+  }
+}
