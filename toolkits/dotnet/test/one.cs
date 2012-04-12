1ace9cbe0324c0ceea93958778189074aff4ebc5
ianibo <ianibbo@googlemail.com>
2012-04-12 13:47:08 +0100

    Test calling search from cs client

--- toolkits/dotnet/test/one.cs
+++ toolkits/dotnet/test/one.cs
@@ -1,6 +1,25 @@
 
+using System;  
+using System.IO;  
+using System.Net;  
+using System.Text;  
 class one {
   static void Main() {
     System.Console.WriteLine("start");
+  
+    // Create the web request  
+    HttpWebRequest request = WebRequest.Create("http://coursedata.k-int.com:9200/courses/course/_search?q=Painting") as HttpWebRequest;  
+  
+    // Get response  
+    using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)  
+    {  
+      // Get the response stream  
+      StreamReader reader = new StreamReader(response.GetResponseStream());  
+  
+      // Console application output  
+      Console.WriteLine(reader.ReadToEnd());  
+    }  
+
+    System.Console.WriteLine("Call complete");
   }
 }
