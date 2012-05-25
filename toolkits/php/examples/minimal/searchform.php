ea49bf6fdefacc3306730e637c0485938b12d2f8
ianibo <ianibbo@googlemail.com>
2012-05-25 17:38:48 +0100

    Added php examples

---
+++ toolkits/php/examples/minimal/searchform.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+  </head>
+  <body>
+    <form action="dosearch.php" method="get">
+      Course Name: <input name="coursename" type="text"/><br/>
+      <input type="submit"/>
+    </form>
+  </body>
+</html>
