b6139e70d8643a5ac496fd41d491a6048c52b49f
Ian Ibbotson <ianibbo@gmail.com>
2012-05-26 12:54:59 +0100

    Working grails/java example

--- toolkits/java-grails/xcrigrails/grails-app/conf/BuildConfig.groovy
+++ toolkits/java-grails/xcrigrails/grails-app/conf/BuildConfig.groovy
@@ -41,6 +41,9 @@ grails.project.dependency.resolution = {
         runtime ":jquery:1.7.1"
         runtime ":resources:1.1.6"
 
+        // compile ":standalone:1.0"
+
+
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
         //runtime ":cached-resources:1.0"
