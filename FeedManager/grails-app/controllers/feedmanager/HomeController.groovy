53ca7a66350e9be2ef72f5a4b77761613dc807af
ianibo <ianibbo@googlemail.com>
2012-06-14 20:52:18 +0100

    Changed last commit to not do a restriction if user is in admin group

--- FeedManager/grails-app/controllers/feedmanager/HomeController.groovy
+++ FeedManager/grails-app/controllers/feedmanager/HomeController.groovy
@@ -7,27 +7,31 @@ import grails.converters.*
 class HomeController {
 
   def index() {
-	  	
-	ShiroUser userInstance = ShiroUser.findByUsername(SecurityUtils.subject?.principal);
-	  
+      
+    ShiroUser userInstance = ShiroUser.findByUsername(SecurityUtils.subject?.principal);
+    
     params.max = Math.min(params.max ? params.int('max') : 10, 100)
-	
+  
     def user_id = userInstance.id
     
-    if(userInstance.roles.contains("Administrator"))
-    {
-        user_id = "*" //we want to return all feeds
+    if(userInstance.roles.contains("Administrator")) {
+        user_id = "%" //we want to return all feeds
     }
     
-	def results = com.k_int.feedmanager.Datafeed.createCriteria().list(max: params.max, offset: params.offset)
-	{
-		and
-		{
-            eq("owner.id", user_id)
-			if(params.sort && params.order) { order(params.sort, params.order)}
-		}
-	}
-	
+    def results = com.k_int.feedmanager.Datafeed.createCriteria().list(max: params.max, offset: params.offset) {
+      and {
+        if ( userInstance.roles.contains("Administrator") ) {
+          // no restriction
+        }
+        else {
+          eq("owner.id", user_id)
+        }
+      }
+      if(params.sort && params.order) { 
+        order(params.sort, params.order)
+      }
+    }
+  
     def response = [:]
     response.feeds = results
     response.feedsTotal = results.totalCount
