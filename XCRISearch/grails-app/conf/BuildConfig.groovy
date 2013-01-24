723ee21b00c901aa28557b411742b8c1167e893d
mantas <mantas@student>
2013-01-24 15:38:42 +0000

    updated to use mongo 1.0 dependency

--- XCRISearch/grails-app/conf/BuildConfig.groovy
+++ XCRISearch/grails-app/conf/BuildConfig.groovy
@@ -34,7 +34,7 @@ grails.project.dependency.resolution = {
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
         runtime 'mysql:mysql-connector-java:5.1.18'
-        runtime 'com.gmongo:gmongo:1.0'
+        runtime 'com.gmongo:gmongo:1.0' 
         runtime 'org.elasticsearch:elasticsearch-lang-groovy:1.1.0'
         runtime 'org.elasticsearch:elasticsearch:0.19.4'
         runtime 'org.apache.tika:tika-core:0.9'
