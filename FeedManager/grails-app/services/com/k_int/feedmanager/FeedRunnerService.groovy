cdc6d59cf4d1a2cf433602fb3432756ff464a62f
Ian Ibbotson <ianibbo@gmail.com>
2013-02-12 23:07:58 +0000

    Added read timeout

--- FeedManager/grails-app/services/com/k_int/feedmanager/FeedRunnerService.groovy
+++ FeedManager/grails-app/services/com/k_int/feedmanager/FeedRunnerService.groovy
@@ -48,7 +48,8 @@ class FeedRunnerService {
         log.debug("Processing single file datafeed. ${feed_definition.baseurl}");
         java.net.URL resource = new java.net.URL(feed_definition.baseurl)
         java.net.URLConnection url_conn = resource.openConnection();
-        url_conn.setConnectTimeout(10000);
+        url_conn.setConnectTimeout(10000); // 10 seconds
+        url_conn.setReadTimeout(300000); // 5mins
         url_conn.connect();
         log.debug("url connection reports content encoding as : ${url_conn.getContentEncoding()}");
         def upload_result = uploadStream(force_process, url_conn.getInputStream(),aggregator_service,feed_definition)
