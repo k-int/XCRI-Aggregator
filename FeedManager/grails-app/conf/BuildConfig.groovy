f6db640ffd4755a89ba7a46931d791fd809c58bc
mantas <mantas@student>
2013-04-19 14:19:16 +0100

    Build Config Fix

--- FeedManager/grails-app/conf/BuildConfig.groovy
+++ FeedManager/grails-app/conf/BuildConfig.groovy
@@ -34,7 +34,7 @@ grails.project.dependency.resolution = {
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-        runtime 'org.elasticsearch:elasticsearch-lang-groovy:1.1.0'
+        runtime 'org.elasticsearch:elasticsearch-lang-groovy:1.3.0'
         runtime 'com.gmongo:gmongo:0.9.2'
         runtime 'mysql:mysql-connector-java:5.1.18'
         runtime 'commons-fileupload:commons-fileupload:1.2.2'
